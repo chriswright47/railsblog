db907a3dbe614fb574f95cd0caac16c061cfc409
Chris Wright <christopher.wright47@gmail.com>
2013-11-17 12:05:26 -0600

    trying to deploy to heroku

--- config/application.rb
+++ config/application.rb
@@ -59,12 +59,12 @@ module Blog
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
-    config.assets.initialize_on_precomplie = false
-
     # Enable the asset pipeline
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precomplie = false
   end
 end
