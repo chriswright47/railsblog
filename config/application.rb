d1f7042829c7bcf55e811c18edb200a6298521ff
Chris Wright <christopher.wright47@gmail.com>
2013-11-17 13:21:24 -0600

    trying to add bootstrap for styling

--- config/application.rb
+++ config/application.rb
@@ -65,6 +65,8 @@ module Blog
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
     config.assets.initialize_on_precompile = false
   end
 end
